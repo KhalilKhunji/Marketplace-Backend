1afd05f09aeafb4a521ac4d8beed09e19ebc8e41
AliHussain <alihussain08950@gmail.com>
2024-09-04 06:04:47 +0300

    authentication controllers file created and authentication controllers imported and applied to the server.js

---
+++ controllers/authenticationControllers.js
@@ -0,0 +1,47 @@
+const User = require("../models/userModel");
+const Profile = require("../models/profileModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// sign up
+const signup = async (req, res) => {
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(
+      req.body.password,
+      Number(process.env.SALT_ROUNDS) || 10,
+    );
+    const user = await User.create({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await Profile.create({ owner: user._id, address: req.body.address });
+    res.status(201).json({ message: "User created" });
+  };
+  
+// sign in
+const signin = async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "User does not exist or invalid credentials" });
+      }
+      const payload = {
+        id: user._id,
+        username: user.username,
+      };
+      const accessToken = jwt.sign(payload, process.env.JWT_SECRET);
+      return res.status(200).json({ message: "Signin successful", accessToken: accessToken });
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: "Error"});
+    }
+  };
+  
+module.exports = { signin, signup };
--- server.js
+++ server.js
@@ -26,45 +26,9 @@ app.get("/", (req, res) => {
 });
 
 // Authentication routes
-
-app.post("/signup", async (req, res) => {
-  const existingUser = await User.findOne({ username: req.body.username });
-  if (existingUser) {
-    return res.status(400).json({ message: "User already exists" });
-  }
-  const hashedPassword = await bcrypt.hash(
-    req.body.password,
-    Number(process.env.SALT_ROUNDS) || 10,
-  );
-  const user = await User.create({
-    username: req.body.username,
-    password: hashedPassword,
-  });
-  await Profile.create({ owner: user._id, address: req.body.address });
-  res.status(201).json({ message: "User created" });
-});
-
-app.post("/signin", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "User does not exist or invalid credentials" });
-    }
-    const payload = {
-      id: user._id,
-      username: user.username,
-    };
-    const accessToken = jwt.sign(payload, process.env.JWT_SECRET);
-    return res.status(200).json({ message: "Signin successful", accessToken: accessToken });
-  } catch (error) {
-    console.log(error)
-    return res.status(500).json({ message: "Error"});
-  }
-});
+const authenticationControllers = require("./controllers/authenticationControllers");
+app.post("/signup", authenticationControllers.signup);
+app.post("/signin", authenticationControllers.signin);
 
 // JWT route protection middleware
 const authenticateToken = require("./middleware/authenticateToken");
