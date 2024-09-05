2db2e614e3b1d520210a61cf2cf42fe4d6866f3c
KhalilKhunji <khalilkhunji@gmail.com>
2024-09-05 11:00:08 +0300

    MFixed some validation for which routes were restricted by user who created the resource, and issue where all comments were deleted instead of a specific comment.

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -2,71 +2,69 @@ const User = require("../models/userModel");
 const Item = require("../models/itemModel");
 
 const create = async (req, res) => {
-    if(String(req.user.id) === req.params.userId){
-      try {
-        const item = await Item.create({
-          name: req.body.name,
-          description: req.body.description,
-          category: req.body.category,
-          price : req.body.price,
-          seller: req.params.userId
-        });
-        res.status(200).json({ item: item });
-      } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: "Server Error" });
-      }
-    }
-  };
-
-const index = async (req, res) => {
-    if(String(req.user.id) === req.params.userId){
+  if (String(req.user.id) === req.params.userId) {
     try {
-      const items = await Item.find();
-      res.status(200).json({ items: items });
-    } catch (error) {
-      res.status(500).json({ error: "Server Error" });
-    }
-  }
-  };
-
-const show =  async (req, res) => {
-    if(String(req.user.id) === req.params.userId){
-    try {
-      const item = await Item.find({_id: req.params.itemId});
-      res.status(200).json({ item: item });
-    } catch (error) {
-      res.status(500).json({ error: "Server Error" });
-    }
-  }
-  };
-
-const update = async (req, res) => {
-    if(String(req.user.id) === req.params.userId){
-    try {
-      const item = await Item.findByIdAndUpdate(req.params.itemId,{
+      const item = await Item.create({
         name: req.body.name,
         description: req.body.description,
         category: req.body.category,
-        price : req.body.price,
-      }, { new: true});
+        price: req.body.price,
+        seller: req.params.userId,
+      });
       res.status(200).json({ item: item });
     } catch (error) {
+      console.log(error);
       res.status(500).json({ error: "Server Error" });
     }
   }
+};
+
+const index = async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).json({ items: items });
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
   }
-const deleteItem = async (req, res) => {
-    if(String(req.user.id) === req.params.userId){
+};
+
+const show = async (req, res) => {
+  try {
+    const item = await Item.find({ _id: req.params.itemId });
+    res.status(200).json({ item: item });
+  } catch (error) {
+    res.status(500).json({ error: "Server Error" });
+  }
+};
+
+const update = async (req, res) => {
+  if (String(req.user.id) === req.params.userId) {
     try {
-      const item = await Item.findByIdAndDelete(req.params.itemId)
+      const item = await Item.findByIdAndUpdate(
+        req.params.itemId,
+        {
+          name: req.body.name,
+          description: req.body.description,
+          category: req.body.category,
+          price: req.body.price,
+        },
+        { new: true }
+      );
       res.status(200).json({ item: item });
     } catch (error) {
       res.status(500).json({ error: "Server Error" });
     }
   }
-  };
+};
+const deleteItem = async (req, res) => {
+  if (String(req.user.id) === req.params.userId) {
+    try {
+      const item = await Item.findByIdAndDelete(req.params.itemId);
+      res.status(200).json({ item: item });
+    } catch (error) {
+      res.status(500).json({ error: "Server Error" });
+    }
+  }
+};
 
-  module.exports = { create, index, show, update, deleteItem };
-
-
+module.exports = { create, index, show, update, deleteItem };
