70eb7b56112cc96639d387a013b44a69a9482bf5
AliHussain <alihussain08950@gmail.com>
2024-09-10 22:51:50 +0300

    Fixed the bug that prevents buying

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -47,24 +47,60 @@ const show = async (req, res) => {
 const update = async (req, res) => {
   if (String(req.user.id) === req.params.userId) {
     try {
-      const item = await Item.findByIdAndUpdate(
-        req.params.itemId,
-        {
-          name: req.body.name,
-          description: req.body.description,
-          category: req.body.category,
-          price: req.body.price,
-          buyer: req.body.buyer,
-          image: req.file.location,
-        },
-        { new: true }
-      );
+      const item = await Item.findById(req.params.itemId);
+      if (!item) {
+        return res.status(404).json({ error: "Item not found" });
+      }
+      if (item.buyer === null) {
+        await Item.findByIdAndUpdate(
+          req.params.itemId,
+          { buyer: req.user.id },
+          { new: true },
+        );
+      }
+      if (req.body.name) {
+        await Item.findByIdAndUpdate(
+          req.params.itemId,
+          { name: req.body.name },
+          { new: true },
+        );
+      }
+      if (req.body.description) {
+        await Item.findByIdAndUpdate(
+          req.params.itemId,
+          { description: req.body.description },
+          { new: true },
+        );
+      }
+      if (req.body.category) {
+        await Item.findByIdAndUpdate(
+          req.params.itemId,
+          { category: req.body.category },
+          { new: true },
+        );
+      }
+      if (req.body.price) {
+        await Item.findByIdAndUpdate(
+          req.params.itemId,
+          { price: req.body.price },
+          { new: true },
+        );
+      }
+      if (req.file && req.file.location) {
+        await Item.findByIdAndUpdate(
+          req.params.itemId,
+          { image: req.file.location },
+          { new: true },
+        );
+      }
+      const updatedItem = await Item.findById(req.params.itemId);
       res.status(200).json({ item: item });
     } catch (error) {
       res.status(500).json({ error: "Server Error" });
     }
   }
 };
+
 const deleteItem = async (req, res) => {
   if (String(req.user.id) === req.params.userId) {
     try {
